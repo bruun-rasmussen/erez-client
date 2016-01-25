211f49f184fc177b3f3f1c302d5d01f4b7fdbded
Ole Sandum <ole@sandum.net>
2016-01-25 13:56:42 +0100

    limit test

--- src/test/java/dk/es/br/erez/Erez3PingTest.java
+++ src/test/java/dk/es/br/erez/Erez3PingTest.java
@@ -50,10 +50,10 @@ public class Erez3PingTest {
      }
      */
 
-    @Test(threadPoolSize = 100, invocationCount = 100, timeOut = 100000)
+    @Test
     public void testTraversal() throws Exception {
-        int n = new EC(locator)._traverse("");
-        Assert.assertEquals(n, 17);
+        int n = new EC(locator)._traverse("BRWeb/Arkiv/Online/518");
+        Assert.assertEquals(n, 1067);
     }
 
     private static class EC {
@@ -63,11 +63,7 @@ public class Erez3PingTest {
 
         EC(Erez3Locator locator) throws Exception {
             erezPort = locator.geterez3Port(erezPortAddress);
-
-            userHandle = erezPort.wsLogin("tintin", "t1n0bPal");
-      //userHandle = erezPort.wsLogin("tintin", "hej");
-//      userHandle = erezPort.wsLogin("bum", "hej");
-            //     userHandle = null;
+            userHandle = erezPort.wsLogin("bum", "hej");
             System.out.println("userHandle: " + userHandle);
 
         }
